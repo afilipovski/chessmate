36c68be332099d21c86603cc439daeed4bfa8a8e
ivancekikj <ivan.cekic10@gmail.com>
2025-02-07 12:53:36 +0100

    Add generic singleton class

---
+++ SingletonBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessMate
+{
+    public abstract class SingletonBase<T> where T : class, new()
+    {
+        private static readonly Lazy<T> _instance = new Lazy<T>(() => new T());
+
+        public static T Instance => _instance.Value;
+
+        protected SingletonBase()
+        {
+            if (_instance.IsValueCreated)
+            {
+                throw new InvalidOperationException("Instance already created.");
+            }
+        }
+    }
+}
